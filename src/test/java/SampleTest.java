43c0191d9aa3d7e57d5ad56de611e17c8f8f18c4
mtotodev05 <mtotodev05@gmail.com>
2018-03-19 22:47:11 +0300

    Pushed Test cases

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author brian-kamau
+ */
+public class SampleTest {
+    
+    public SampleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void TestClass(){
+        boolean expectedResult = true;
+        boolean result = true;
+        assertEquals(expectedResult,result);
+    }
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
